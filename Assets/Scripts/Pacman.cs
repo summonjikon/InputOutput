5ca14b1dab4717c26f37d5ec88776d8525114257
Joost-Robat <30769@ma-web.nl>
2022-03-22 14:49:42 +0100

    Updated Pacman, can now be controlled using the motion sensor and arduino setup.

--- Assets/Scripts/Pacman.cs
+++ Assets/Scripts/Pacman.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
-
+using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
 [RequireComponent(typeof(Movement))]
 public class Pacman : MonoBehaviour
 {
@@ -7,28 +11,59 @@ public class Pacman : MonoBehaviour
     public SpriteRenderer spriteRenderer { get; private set; }
     public new Collider2D collider { get; private set; }
     public Movement movement { get; private set; }
-
+    public SerialPort port = new SerialPort("COM6", 19200);
+    string lastInput;
+    string updatedInput;
+    float timer;
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         collider = GetComponent<Collider2D>();
         movement = GetComponent<Movement>();
+        port.Open();
+
+        port.ReadTimeout = 100;
     }
 
     private void Update()
     {
         // Set the new direction based on the current input
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
-            movement.SetDirection(Vector2.up);
+        if(timer > 1)
+        {
+            if (port.IsOpen == true)
+            {
+                updatedInput = port.ReadLine();
+                if (updatedInput != lastInput)
+                {
+                    lastInput = updatedInput;
+                    //up
+                    if (lastInput == "UP")
+                    {
+                        movement.SetDirection(Vector2.up);
+                    }
+                    //down
+                    else if (lastInput == "DOWN")
+                    {
+                        movement.SetDirection(Vector2.down);
+                    }
+                    //left
+                    else if (lastInput == "LEFT")
+                    {
+                        movement.SetDirection(Vector2.left);
+                    }
+                    //right
+                    else if (lastInput == "RIGHT")
+                    {
+                        movement.SetDirection(Vector2.right);
+                    }
+                    timer = 0;
+                }
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
-            movement.SetDirection(Vector2.down);
-        }
-        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-            movement.SetDirection(Vector2.left);
-        }
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
-            movement.SetDirection(Vector2.right);
+
+        else
+        {
+            timer += Time.deltaTime;
         }
 
         // Rotate pacman to face the movement direction
